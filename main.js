6f5053bbc9a5eb43810c57843af35460f55e93e0
Christian Kaltenbach <christian123kalt@gmail.com>
2023-04-21 17:58:01 +0200

    sprint01

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const port = 3000,
- http = require("http"),
- httpStatus = require("http-status-codes"),
- app = http
- .createServer((req, res) => {
-res.writeHead(httpStatus.OK, {
-"Content-Type": "text/html"
+const port = 3000;
+http = require("http");
+httpStatus = require("http-status-codes");
+
+app = http.createServer();
+
+app.on("request", (req, res) => {
+    console.log(req.url);
+    res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+    });
+    let responseMessage = "<h1>Willkommen beim Sprint01!</h1>";
+    res.end(responseMessage);
 });
-let responseMessage = "<h1>Willkomen zum Sprint01!</h1>";
-res.end(responseMessage);
- })
- .listen(port);
+
+app.listen(port);
